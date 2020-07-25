7fc5ec1c3469c36f3d8ef77e7f3fd3b533434b37
Rafael Escobar <rescobar@redventures.com>
2020-07-24 23:27:26 -0300

    Stop filtering by headers

--- cmd/api/server/middleware/cors.go
+++ cmd/api/server/middleware/cors.go
@@ -9,7 +9,6 @@ import (
 func CORSMiddleware(serverAuthClientsURLs []string) RequestMiddleware {
 	return cors.NewCorsHandler(cors.Options{
 		AllowedOrigins:   serverAuthClientsURLs,
-		AllowedHeaders:   []string{"Origin", "Accept", "Content-Type"},
 		AllowedMethods:   []string{http.MethodGet, http.MethodPost, http.MethodPut},
 		AllowCredentials: false,
 		AllowMaxAge:      5600,
