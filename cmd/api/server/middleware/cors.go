d7b7a604a33da2a9c63528351b7ad11207e93232
Rafael Escobar <rescobar@redventures.com>
2020-07-24 23:14:05 -0300

    Debug

--- cmd/api/server/middleware/cors.go
+++ cmd/api/server/middleware/cors.go
@@ -9,9 +9,10 @@ import (
 func CORSMiddleware(serverAuthClientsURLs []string) RequestMiddleware {
 	return cors.NewCorsHandler(cors.Options{
 		AllowedOrigins:   serverAuthClientsURLs,
-		AllowedHeaders:   []string{"Content-Type"},
+		AllowedHeaders:   []string{"Origin", "Accept", "Content-Type"},
 		AllowedMethods:   []string{http.MethodGet, http.MethodPost, http.MethodPut},
 		AllowCredentials: false,
 		AllowMaxAge:      5600,
+		Debug:            true,
 	}).CorsMiddleware
 }
